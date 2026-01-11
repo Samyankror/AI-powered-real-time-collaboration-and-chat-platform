1ce8834429740e467021a6dc7c0c4ce0823e3498
Samyank ror <samyankror77@gmail.com>
2026-01-12 00:22:01 +0530

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    },
-    proxy :{
-      '/api':{
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure : false,
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      headers: {
+        "Cross-Origin-Embedder-Policy": "require-corp",
+        "Cross-Origin-Opener-Policy": "same-origin"
+      },
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,   // ✅ now works
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  }
-})
+  };
+});
+
