9550b111d51e74ec5ef3515761c8629db79ce674
Samyank ror <samyankror77@gmail.com>
2026-06-14 20:23:34 +0530

    Delete AI_powered directory

--- AI_powered/backend/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { autoFix, getResult } from "../controllers/ai.controller.js";
-
-const router = Router();
-router.get("/get-result", getResult);
-router.post("/autofix",autoFix);
-
-export default router;
